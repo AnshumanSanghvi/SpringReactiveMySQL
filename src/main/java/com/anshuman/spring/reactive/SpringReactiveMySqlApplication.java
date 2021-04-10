ff1bc4dfdf5708a40f6536e6cc28e7c3f34275fc
Anshuman Sanghvi <anshuman.sanghvi@brevitaz.com>
2021-04-10 23:44:38 +0530

    Setup Spring Boot App

---
+++ src/main/java/com/anshuman/spring/reactive/SpringReactiveMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.anshuman.spring.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveMySqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anshuman/spring/reactive/SpringReactiveMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anshuman.spring.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
